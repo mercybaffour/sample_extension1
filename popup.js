2c157e02571471a4ca434e0fad8c715e4580cb7d
Mercy Baffour <mercy.baffour@gmail.com>
2021-11-13 09:59:43 -0500

    "sample extension tutorial"

---
+++ popup.js
@@ -0,0 +1,26 @@
+// Initialize button with user's preferred color
+let changeColor = document.getElementById("changeColor");
+
+chrome.storage.sync.get("color", ({ color }) => {
+  changeColor.style.backgroundColor = color;
+});
+
+//This code grabs the button from popup.html and requests the color value from storage. It then applies the color as the background of the button (W@ITHOUT BUTTON CLICK)
+
+// When the button is clicked, inject setPageBackgroundColor (function) into current page
+changeColor.addEventListener("click", async () => {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      function: setPageBackgroundColor,
+    });
+  });
+  
+  // The body of this function will be executed as a content script inside the
+  // current page
+  function setPageBackgroundColor() {
+    chrome.storage.sync.get("color", ({ color }) => {
+      document.body.style.backgroundColor = color;
+    });
+  }
